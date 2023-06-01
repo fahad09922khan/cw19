19ecbce7453f44498e0c976cc2e67fe66452a8a8
fahad khan <fahadkhan09922@gmail.com>
2023-06-01 17:06:26 +0500

    update

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = ['index.html',
+    'product.js',
+    'petstore1.webmanifest',
+    'images/yarn.jpg',
+    'images/cat-litter.jpg',
+    'images/laser-pointer.jpg',
+    'images/cat-house.jpg',
+    'images/icon-store-512.png'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(caches.open(cacheName).then((cache) => {
+        console.log('[Service Worker] Caching all the files');
+        return cache.addAll(cacheFiles);
+    }));
+});
